0950acbc2bbc23526b330297efecb5205ebc1cbb
min950903 <min950903@gmail.com>
2020-10-12 23:46:49 +0900

    Updates

---
+++ precache-manifest.96fe79cf4d66601d259546e92250358c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6734ca87334a06b368134499b0c88c",
+    "url": "/habit-tracker/index.html"
+  },
+  {
+    "revision": "43a428c417a1b481c048",
+    "url": "/habit-tracker/static/css/main.39b2d0d0.chunk.css"
+  },
+  {
+    "revision": "e9e08c281eaed7696b59",
+    "url": "/habit-tracker/static/js/2.edd41bd7.chunk.js"
+  },
+  {
+    "revision": "746582258d70325461391e08406982a5",
+    "url": "/habit-tracker/static/js/2.edd41bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a428c417a1b481c048",
+    "url": "/habit-tracker/static/js/main.b594de56.chunk.js"
+  },
+  {
+    "revision": "5d759b3fde7c32f59156",
+    "url": "/habit-tracker/static/js/runtime-main.0e3ad41c.js"
+  }
+]);
